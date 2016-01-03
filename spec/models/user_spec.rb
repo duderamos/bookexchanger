b1c5e5c5daa8cf3a69ff645400a8a8ca9da553a2
Eduardo Ramos <ramos.eduardo87@gmail.com>
2016-01-03 00:56:58 -0200

    Adding spec to model User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+RSpec.describe User, type: :model do
+  subject { build :user }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:role) }
+end
